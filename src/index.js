624307163b0910b4c7ead35acb967406449b52dd
Nataly <nataly87s@gmail.com>
2016-09-09 09:50:28 +0300

    add routing to components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import DrawCanvas from './DrawCanvas';
+import ControlCanvas from './ControlCanvas';
+
+const NoMatch = props => <div>:(</div>
+const App = props => 
+    <div>
+        <ul>
+            <li><Link to={'/draw'}>Draw</Link></li>
+            <li><Link to={'/control'}>Control</Link></li>
+        </ul>
+        {props.children}
+    </div>
+  
+  
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={NoMatch} />
+            <Route path="control" component={ControlCanvas}/>
+            <Route path="draw" component={DrawCanvas}/>
+            <Route path="*" component={NoMatch}/>
+        </Route>
+    </Router>
+  ), document.getElementById('root'));
