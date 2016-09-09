a816f7b652bc4ab32d1e89a69dfba4ac1695ec6f
Nataly <nataly87s@gmail.com>
2016-09-09 13:16:12 +0300

    fix control trail

--- src/ControlStore.js
+++ src/ControlStore.js
@@ -10,15 +10,19 @@ export default class extends CursorStore {
         const disposable = this.onMouseMove
             .map(position => {
                 const index = this.trail.length === 0 ? 0 : this.trail[this.trail.length-1].index +1;
-                return {position, index, opacity: 1};
+                return {position, index, opacity: 1, show: true};
             })
             .doOnNext(item => this.trail.push(item))
+            .flatMap(x => Rx.Observable.just(x).delay(100))
+            .doOnNext(item => item.show = false)
             .flatMap(x => {
+                const observable = Rx.Observable.just(x);
+                if (this.trail.filter(t => t.show).length > 0) return observable;
                 return this.subject
                             .take(1)
                             .takeUntil(this.mouseSubject.filter(x => !x))
                             .last({defaultValue: 1})
-                            .flatMap(_=>Rx.Observable.just(x).delay(100));
+                            .flatMap(_=>observable);
             })
             .doOnNext(x => x.opacity = 0)
             .flatMap(x => Rx.Observable.just(x).delay(500))
